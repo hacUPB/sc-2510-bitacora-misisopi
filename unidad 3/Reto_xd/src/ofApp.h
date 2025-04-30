f8c443537d88713f9c000f6ba7af79e5474c8c4b
Misisopi <ana.misasg@upb.edu.co>
2025-04-30 00:27:06 -0500

    correcion

--- unidad 3/Reto_xd/src/ofApp.h
+++ unidad 3/Reto_xd/src/ofApp.h
@@ -7,17 +7,27 @@ public:
     void setup();
     void update();
     void draw();
-    void mousePressed(int x, int y, int button);
+    void mousePressed(int x, int y, int button); // Evento al presionar el mouse
 
 private:
-    void generateGrid();
-    void convertMouseToRay(int x, int y, glm::vec3& rayStart, glm::vec3& rayEnd);
+    void generateGrid(); // Genera la grilla de esferas en 3D
+    void convertMouseToRay(int x, int y, glm::vec3& rayStart, glm::vec3& rayEnd); // Convierte el clic del mouse en un rayo 3D
     bool rayIntersectsSphere(const glm::vec3& rayStart, const glm::vec3& rayDir,
         const glm::vec3& sphereCenter, float sphereRadius,
-        glm::vec3& intersectionPoint);
+        glm::vec3& intersectionPoint); // Detecta colisión entre un rayo y una esfera
 
-    ofEasyCam cam;
-    std::vector<glm::vec3> spherePositions;
-    glm::vec3 selectedSphere;
-    float distDiv, amplitud, step;
+    /*rayStart: el origen del rayo (desde dónde sale).
+
+     rayDir: la dirección del rayo (normalmente es rayEnd - rayStart).
+
+     sphereCenter: el centro de la esfera.
+
+     sphereRadius: el radio de la esfera.
+
+     intersectionPoint: si hay colisión, aquí se guarda el punto exacto donde el rayo toca la esfera.*/
+
+    ofEasyCam cam; //camara interactiva 
+    std::vector<glm::vec3> spherePositions; // las posiciones de todas las esferas 
+    glm::vec3 selectedSphere; // la esfera seleccionada intersectada 
+    float distDiv, amplitud, step; //los parametros q calculan el grind
 };
